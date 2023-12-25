a8f771bbc5faa16ce90f6c883ea2a6e022a061a3
Amish <amish.tufail2002@gmail.com>
2023-12-25 17:39:11 +0500

    Initial Commit

---
+++ NavigationAPIPractice/NavigationAPIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAPIPracticeApp.swift
+//  NavigationAPIPractice
+//
+//  Created by Amish Tufail on 25/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationAPIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
